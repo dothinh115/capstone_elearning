b466c0f8132a87de6e85320b42dec40eb2d784d7
dothinh115 <dothinh115@gmail.com>
2023-01-16 11:44:59 +0700

    up

--- src/util/function.ts
+++ src/util/function.ts
@@ -1,13 +1,12 @@
 export const { suffleArray } = {
   suffleArray<T>(arr: T[], limit: number): T[] {
     arr = [...arr];
-    let result: T[] = [],
-      i: number = 0;
-    while (i < limit) {
+    let result: T[] = [];
+    while (limit !== 0) {
       let randomIndex: number = Math.floor(Math.random() * arr.length);
       result.push(arr[randomIndex]);
       arr.splice(randomIndex, 1);
-      i++;
+      limit--;
     }
     return result;
   },
