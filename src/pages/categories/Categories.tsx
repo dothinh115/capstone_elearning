6c92d64f2f5fa0f6bcbf720f65f33f9e52e3dfa8
dothinh115 <dothinh115@gmail.com>
2023-01-19 06:16:58 +0700

    up

--- src/pages/categories/Categories.tsx
+++ src/pages/categories/Categories.tsx
@@ -95,113 +95,111 @@ const Categories = (props: Props) => {
   }, [searchParams]);
 
   return (
-    <>
-      <section className="categories">
-        <div className="categories_container">
-          <div className="categories_container_sidebar">
-            <div className="categories_container_sidebar_inner">
-              <div className="categories_container_sidebar_inner_header">
-                <h1>
-                  <i className="fa-solid fa-list"></i>Bộ lọc
-                </h1>
-              </div>
-              <div className="categories_container_sidebar_inner_body">
-                <h1>Tất cả danh mục</h1>
-                <ul>
-                  {categories?.map((item: CategoriesType, index: number) => {
-                    return (
-                      <li key={index}>
-                        <input
-                          type="checkbox"
-                          value={item.maDanhMuc}
-                          onChange={checkboxHandle}
-                          defaultChecked={checkCheked(item.maDanhMuc)}
-                        />
-                        {item.tenDanhMuc}
-                      </li>
-                    );
-                  })}
-                </ul>
-              </div>
+    <section className="categories">
+      <div className="categories_container">
+        <div className="categories_container_sidebar">
+          <div className="categories_container_sidebar_inner">
+            <div className="categories_container_sidebar_inner_header">
+              <h1>
+                <i className="fa-solid fa-list"></i>Bộ lọc
+              </h1>
             </div>
-          </div>
-          <div className="categories_container_main">
-            <div className="categories_container_main_body">
-              <div className="selectedCategories">
-                <i className="fa-solid fa-arrow-right"></i>
-                Khóa học đã chọn:{" "}
-                {checkedCategories?.map((item: string, index: number) => {
-                  const find = categories?.find(
-                    (val: CategoriesType) => val.maDanhMuc === item
-                  );
+            <div className="categories_container_sidebar_inner_body">
+              <h1>Tất cả danh mục</h1>
+              <ul>
+                {categories?.map((item: CategoriesType, index: number) => {
                   return (
-                    find !== undefined && (
-                      <span
-                        className={`badge badge-${randomBadgeArr[index]}`}
-                        key={index}
-                      >
-                        {find.tenDanhMuc}
-                      </span>
-                    )
+                    <li key={index}>
+                      <input
+                        type="checkbox"
+                        value={item.maDanhMuc}
+                        onChange={checkboxHandle}
+                        defaultChecked={checkCheked(item.maDanhMuc)}
+                      />
+                      {item.tenDanhMuc}
+                    </li>
                   );
                 })}
-              </div>
-
-              {coursesByCategories()!?.length === 0 ? (
-                <h1 className="notfound">
-                  <img src="../../img/notfound.png" alt="" />
-                  <div>Chưa có gì ở đây!</div>
-                </h1>
-              ) : (
-                <div className="selectedCategories result">
-                  <i className="fa-solid fa-arrow-right"></i>
-                  Đã tìm thấy:{" "}
-                  <span className="badge badge-info">
-                    {coursesByCategories()!?.length}
-                  </span>{" "}
-                  kết quả.
-                </div>
-              )}
-              {coursesByCategories()!?.length <= limitCouses ? (
-                <>
-                  {coursesByCategories()?.map(
-                    (item: CourseType, index: number) => {
-                      return <CardItem item={item} key={index} />;
-                    }
-                  )}
-                </>
-              ) : (
-                <>
-                  {coursesByCategories()
-                    ?.slice(0, limitCouses)
-                    .map((item: CourseType, index: number) => {
-                      return <CardItem item={item} key={index} />;
-                    })}
-                </>
-              )}
-
-              {coursesByCategories()!?.slice(0, limitCouses).length >=
-                limitCouses && (
-                <div className="categories_container_main_body_btn">
-                  <button
-                    className="btn"
-                    onClick={() => {
-                      dispatch(
-                        setLimitCoursesAction(
-                          limitCouses + limitCategoriesCoursesViewMore
-                        )
-                      );
-                    }}
-                  >
-                    Xem thêm
-                  </button>
-                </div>
-              )}
+              </ul>
             </div>
           </div>
         </div>
-      </section>
-    </>
+        <div className="categories_container_main">
+          <div className="categories_container_main_body">
+            <div className="selectedCategories">
+              <i className="fa-solid fa-arrow-right"></i>
+              Khóa học đã chọn:{" "}
+              {checkedCategories?.map((item: string, index: number) => {
+                const find = categories?.find(
+                  (val: CategoriesType) => val.maDanhMuc === item
+                );
+                return (
+                  find !== undefined && (
+                    <span
+                      className={`badge badge-${randomBadgeArr[index]}`}
+                      key={index}
+                    >
+                      {find.tenDanhMuc}
+                    </span>
+                  )
+                );
+              })}
+            </div>
+
+            {coursesByCategories()!?.length === 0 ? (
+              <h1 className="notfound">
+                <img src="../../img/notfound.png" alt="" />
+                <div>Chưa có gì ở đây!</div>
+              </h1>
+            ) : (
+              <div className="selectedCategories result">
+                <i className="fa-solid fa-arrow-right"></i>
+                Đã tìm thấy:{" "}
+                <span className="badge badge-info">
+                  {coursesByCategories()!?.length}
+                </span>{" "}
+                kết quả.
+              </div>
+            )}
+            {coursesByCategories()!?.length <= limitCouses ? (
+              <>
+                {coursesByCategories()?.map(
+                  (item: CourseType, index: number) => {
+                    return <CardItem item={item} key={index} />;
+                  }
+                )}
+              </>
+            ) : (
+              <>
+                {coursesByCategories()
+                  ?.slice(0, limitCouses)
+                  .map((item: CourseType, index: number) => {
+                    return <CardItem item={item} key={index} />;
+                  })}
+              </>
+            )}
+
+            {coursesByCategories()!?.slice(0, limitCouses).length >=
+              limitCouses && (
+              <div className="categories_container_main_body_btn">
+                <button
+                  className="btn"
+                  onClick={() => {
+                    dispatch(
+                      setLimitCoursesAction(
+                        limitCouses + limitCategoriesCoursesViewMore
+                      )
+                    );
+                  }}
+                >
+                  Xem thêm
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
