1ba000e782c371f8556612f2f73c74c1dea46f53
dothinh115 <dothinh115@gmail.com>
2023-01-19 06:52:21 +0700

    up

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,20 +1,50 @@
 import React from "react";
-import Mainblock from "../../components/mainblock/Mainblock";
+import { Link } from "react-router-dom";
 
 type Props = {};
 
 const Login = (props: Props) => {
   return (
-    <>
-      <Mainblock
-        value="Nếu chưa có Tài khoản, hãy đăng ký!"
-        icon="fa-arrow-right"
-      />
+    <section className="login">
+      <div className="login_header">
+        <h1>ĐĂNG NHẬP</h1>
+        <Link to="/" className="badge">
+          <i className="fa-solid fa-x"></i>
+        </Link>
+      </div>
 
-      <Mainblock value="Đăng nhập thất bại" icon="fa-xmark" iconColor="red" />
-
-      <Mainblock headerContent="Đăng nhập" value="Form đăng nhập ở đây" />
-    </>
+      <div className="login_container">
+        <form>
+          <div className="item">
+            <div className="item_title">
+              <i className="fa-solid fa-envelope"></i>Email:
+            </div>
+            <div className="item_input">
+              <input type="text" />
+            </div>
+          </div>
+          <div className="item">
+            <div className="item_title">
+              <i className="fa-solid fa-lock"></i>Password:
+            </div>
+            <div className="item_input">
+              <input type="text" />
+            </div>
+          </div>
+          <div className="item_btn">
+            <button className="btn btn-primary">Đăng nhập</button>
+          </div>
+        </form>
+      </div>
+      <div className="login_footer">
+        <div className="hr_span_footer">
+          <span>Hoặc</span>
+          <div>
+            <button className="btn">Đăng ký</button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
