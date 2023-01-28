99e6dc4ba1384232df9c92c12f47ce65425d63f7
dothinh115 <dothinh115@gmail.com>
2023-01-28 21:14:23 +0700

    up

--- src/pages/profile/CoursesManage.tsx
+++ src/pages/profile/CoursesManage.tsx
@@ -10,7 +10,7 @@ const CoursesManage = (props: Props) => {
   const { coursesArr } = useSelector(
     (store: ReduxRootType) => store.courseReducer
   );
-  const [resultNumber, setResultNumber] = useState<number>(5);
+  const [resultNumber, setResultNumber] = useState<number>(10);
   const searchValue = useRef<HTMLInputElement | null>(null);
   const [searchResult, setSearchResult] = useState<
     CourseType[] | null | undefined
@@ -38,6 +38,12 @@ const CoursesManage = (props: Props) => {
   return (
     <>
       <div className="profile_container_main_block">
+        <button className="btn">Thêm khóa học mới</button>
+      </div>
+      <div
+        className="profile_container_main_block"
+        style={{ padding: "unset" }}
+      >
         <form
           className="profile_container_main_block_search"
           onSubmit={searchSubmitHandle}
@@ -72,13 +78,19 @@ const CoursesManage = (props: Props) => {
                       }}
                     />
                     <div className="course_info">
-                      <h3>{course.tenKhoaHoc}</h3>
+                      <h3>
+                        {course.tenKhoaHoc?.length > 30
+                          ? course.tenKhoaHoc.substring(0, 29) + "..."
+                          : course.tenKhoaHoc}
+                      </h3>
                       <p>
-                        {course.moTa?.length > 50
-                          ? course.moTa.substring(0, 49) + "..."
+                        {course.moTa?.length > 40
+                          ? course.moTa.substring(0, 39) + "..."
                           : course.moTa}
                       </p>
                       <p>
+                        <i className="fa-solid fa-user"></i>
+                        {course.nguoiTao.hoTen + " | "}
                         <i className="fa-solid fa-calendar-days"></i>
                         {course.ngayTao
                           .substring(0, 10)
@@ -88,6 +100,9 @@ const CoursesManage = (props: Props) => {
                       </p>
                     </div>
                   </Link>
+                  <button>
+                    <i className="fa-solid fa-gear"></i>
+                  </button>
                   <button>
                     <i className="fa-solid fa-trash"></i>
                   </button>
