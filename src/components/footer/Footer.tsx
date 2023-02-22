ddb8f1727e69b99c18ac2cb510b48c98ac94175c
nguyentrihau <nguyenchonhau@gmail.com>
2023-02-23 01:13:27 +0700

    up

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -15,28 +15,29 @@ const Footer = (props: Props) => {
             maximus, nulla utlaoki comm odo sagittis.
           </p>
           <p>
-            © 2022 CyberSoft Capstone Project by group 5 with love{" "}
-            <i className="fa-solid fa-heart" style={{ color: "red" }}></i>.
+            © 2022 CyberSoft Capstone Dự án của nhóm 5 với đầy ắp tình yêu lập
+            trình <i className="fa-solid fa-heart" style={{ color: "red" }}></i>
+            .
           </p>
         </div>
         <div className="footer_inner_center">
-          <h1>About Us</h1>
+          <h1>Về chúng tôi</h1>
           <ul>
-            <li>About us</li>
-            <li>Courses</li>
-            <li>Events</li>
-            <li>Become a teacher</li>
+            <li>Về chúng tôi</li>
+            <li>Khóa học</li>
+            <li>Sự Kiện</li>
+            <li>Trở thành Giáo Viên</li>
           </ul>
         </div>
         <div className="footer_inner_right">
-          <h1>Sign Up for Our Newsletter</h1>
+          <h1>Đăng ký nhận bản tin của chúng tôi</h1>
           <p>
-            Receive weekly newsletter with educational materials, popular books
-            and much more!
+            Nhận bản tin hàng tuần với các tài liệu giáo dục, sách nổi tiếng và
+            nhiều hơn nữa!
           </p>
           <form>
-            <input type="text" placeholder="Your Email" />
-            <button className="btn btn-primary">Send</button>
+            <input type="text" placeholder="Nhập Email của bạn" />
+            <button className="btn btn-primary">Gửi</button>
           </form>
           <div className="footer_inner_right_icon">
             <i className="fa-brands fa-facebook"></i>
